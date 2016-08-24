116525debf099388fc471ee100aa558fd7b1928e
Andrew Bentley <andrew.t.bentley@gmail.com>
2016-08-24 21:27:10 +1000

    Add --list option for listing available tasks for running

--- plank/cli.py
+++ plank/cli.py
@@ -1,10 +1,23 @@
 import click
+from plank import Inspector
 
 from .runner import Runner
 
 
+def list_available_tasks(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    tasks = Inspector().get_tasks()
+    print 'Available tasks:'
+    for task in tasks.values():
+        print '\t{0}'.format(task.name)
+    ctx.exit()
+
+
 @click.command()
 @click.argument('task')
+@click.option('-l', '--list', is_flag=True, is_eager=True, expose_value=False, callback=list_available_tasks,
+              help='List available tasks')
 def main(task):
     runner = Runner()
     runner.run(task)
