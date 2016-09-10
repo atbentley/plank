918dc8c085cea80b7f8aa8fd6de2a8fd0074f997
Andrew Bentley <andrew.t.bentley@gmail.com>
2016-09-10 20:41:39 +1000

    Add the cwd into the python path so the planks module can be found correctly

--- plank/cli.py
+++ plank/cli.py
@@ -1,4 +1,7 @@
+import os
+
 import click
+import sys
 from plank import Inspector
 
 from .runner import Runner
@@ -19,8 +22,10 @@ def list_available_tasks(ctx, param, value):
 @click.option('-l', '--list', is_flag=True, is_eager=True, expose_value=False, callback=list_available_tasks,
               help='List available tasks')
 def main(task):
+    sys.path.insert(0, os.getcwd())
     runner = Runner()
     runner.run(task)
+    sys.path.pop(0)
 
 
 if __name__ == '__main__':
